1d1c87f090a2433aa24a140f6f684519a2bda234
Xadson Lázaro Relógio <xadsonlazaro007@gmail.com>
2025-07-08 09:15:16 -1200

    big project

--- challenges.js
+++ challenges.js
@@ -1,19 +1,13 @@
 [ 
     { 
-         "title": "Inverter String", 
-         "description": "Receba uma string e retorne ela invertida. Exemplo: 'abc" ,
-         "expected": "cba"
-    }, 
-    { 
         "title": "Dobrar número",
-        "Description": "Receba um número e retorne ele Dobrado. Exemplo: 5 -> 10",
-          "input": 5, 
-          "expected": 10
+        "description": "Receba um Numero e retorne ele Dobrado. Ex: 5 -> 10",
+        "input": 5,
+        "expected": 10
     }, 
     { 
-        "title": "Contar caracteres", 
-        "description": "Recebe uma string e retorne o número de caracteres.", 
-        "input": "Hello",
-        "expected": 5
+        "title": "Inverter string", 
+        "descrition": "Receba uma string e retorne ela invertida. Ex: 'abc'", 
+        "expected": "cba"
     }
 ]
--- server.js
+++ server.js
@@ -1,84 +1,63 @@
+let challenges = [];
+let currentChallenge = 0; 
+let score = 0;
 
-let challenges = []; 
-let currentChallenge = 0;
-let score = 0; 
-
-const title = document.getElementById("challenge-title"); 
+const titleEl = document.getElementById("challenge-title");
 const descEl = document.getElementById("challenge-description")
-const nexBtn = document.getElementById("next-challenge") 
-const edition = document.getElementById("run-code")
-const resulEl = document.getElementById("result-message")
+const nextBtn = document.getElementById("next-challenge")
+const runBtn = document.getElementById("run-code")
+const edition = document.getElementById("run-code") 
+const resultEl = document.getElementById("result-message")
 const scoreEl = document.getElementById("score")
 
-
-fetch("challenges.json")
+fetch("challenges.json") 
 .then(res => res.json())
-.then(data => { 
-    challenges = data;
-    loadChallenge(); 
+.then(date => { 
+    challenges = date;
+    loadChallenge();
+})
+.catch(err => { 
+    console.error("Falha ao carregar desafios:", err); 
+    resultEl.textContent = "Erro ao carregar desafios."
 }); 
 
-function loadChallenge () { 
+
+function loadChallenge() { 
     const c = challenges[currentChallenge]; 
-    titleEl.textContent = `Desafios: ${c.title}`;
+    titleEl.textContent = `Desafio: ${c.title}`; 
     descEl.textContent = c.description; 
-    edition.value = `function solve(input) {\n return \n}`;
-    resulEl.textContent = "";
+    
+    edition.value = `function solve(input) {\n return; \n}`;
+    resultEl.textContent = "";
 }
 
-nexBtn.addEventListener("click", () => { 
-    currentChallenge =  
+nextBtn.addEventListener("click", () => { 
+    currentChallenge = 
     (currentChallenge + 1) % 
-    challenges.length;
-    loadChallenge();
-}); 
+    loadChallenge(); 
+});
 
 runBtn.addEventListener("click", () => { 
-    const userCode = editor.value;
-    try { 
-        const func = new 
-        Function("input", userCode + `\nreturn solve(input);`);
+    const userCode = edition.value;
+
+    try {
+        const func = new Function("input", userCode + "\nreturn solve(input);");
+
         const input = challenges[currentChallenge].input; 
-        const expected = challenges[currentChallenge].expected; 
+        const expected = challenges[currentChallenge].expected
 
         const result = func(input); 
 
         if (JSON.stringify(result) === JSON.stringify(expected)) { 
-            resulEl.textContent = " ✔ Correto!"; 
+            result.textContent = "✅ Correto!"
+
             score += 10; 
-            scoreEl.textContent = score;
+            scoreEl.textContent = score; 
              } else { 
-                resulEl.textContent = ` Errado. Resultado: ${resulEl}, Esperado: ${expected}`;
+                resultEl.textContent = `❌ Errado. Recebido: ${result}, 
+                Esperado: ${expected}`
         }
-         } catch (err) { 
-            resulEl.textContent = "  Erro no seu codigo" + err.message;
+    } catch (err) { 
+        resultEl.textContent = "⚠ Erro no codigo" + err.message;
          }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- <input placeholder="Email" type="email" required >
-                <img width="20" height="20"  src="https://img.icons8.com/ios/50/user--v1.png" alt="user--v1"/> 
-                
-
-
-                  <input placeholder="Senha" type="password" required> 
-                <img width="20" height="20" src="https://img.icons8.com/windows/32/lock-2--v1.png" alt="lock-2--v1">
-                <a href="#"> 
-                */  
-
+})
